b9d6085adfbe2bf03cdc854b61ce44954f0ddea6
Kevin Poorman <kpoorman@salesforce.com>
2021-05-19 10:23:49 -0400

    Initial Commit

---
+++ playground/playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  playgroundApp.swift
+//  playground
+//
+//  Created by Kevin Poorman on 5/19/21.
+//
+
+import SwiftUI
+
+@main
+struct playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
