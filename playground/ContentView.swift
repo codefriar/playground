36acca3dec31f5c82775834748baeddc89fdc19e
Kevin Poorman <kjp@codefriar.com>
2021-05-19 23:59:20 -0400

    publisher

--- playground/ContentView.swift
+++ playground/ContentView.swift
@@ -11,77 +11,33 @@ import CoreGraphics
 
 struct ContentView: View {
     @State private var window: NSWindow?
-    let screen = Screen2()
     
     var body: some View {
         VStack {
-            Text("Loading...")
+//            Text("Loading...")
             if nil != window {
-                MainView(store: Store(window: window!))
+                MainView(screen: Screen2(window: window!))
             }
         }.background(WindowGetter(window: $window))
     }
 }
 
 struct MainView: View {
-    var image = CGImage(windowListFromArrayScreenBounds: CGRect(x: 0,y: 0,width: 0,height: 0), windowArray: nil, imageOption: .nil)
-    init(store: Store){
-        let options = CGWindowListOption(arrayLiteral: CGWindowListOption.excludeDesktopElements, CGWindowListOption.optionOnScreenOnly)
-        let windowListInfo = CGWindowListCopyWindowInfo(options, CGWindowID(0))
-        let infoList = windowListInfo as NSArray? as? [[String: AnyObject]]
-        if let il = infoList {
-            for win in il {
-                if let name = win["kCGWindowOwnerName"] as? String,
-                   let bounds = win["kCGWindowBounds"]{
-                    if name == "playground" {
-                        print("Bounds are: \(bounds)")
-                    }
-                }
-            }
-        }
-        self.store = store
-        
-        let windowId = CGWindowID(self.store.window.windowNumber)
-        let windowOption: CGWindowListOption = .optionOnScreenBelowWindow
-        let imageOption: CGWindowImageOption = .boundsIgnoreFraming
-        
-        if let windowCoordinates = self.store.window.contentView?.bounds {
-            let screenCoordinates = self.store.window.convertToScreen(windowCoordinates)
-            let q1Toq4Coordinates = Q1ToQ4Rect(screenCoordinates)
-            if let img = CGWindowListCreateImage(q1Toq4Coordinates, windowOption, windowId, imageOption) {
-                self.image = img
-            }
-        }//view.convert(view.bounds, to: nil)
-    }
+    let screen: Screen2
     
-    let store: Store
+    //var image = CGImage(windowListFromArrayScreenBounds: CGRect(x: 0,y: 0,width: 0,height: 0), windowArray: nil, imageOption: .nil)
+    init(screen: Screen2){
+        self.screen = screen
+    }
     
     var body: some View {
         VStack {
-            Text("MainView with Window: \(store.window)")
-            Image(nsImage: NSImage(cgImage: self.image, size: .zero))
+//            Text("MainView with Window: \(screen.window)")
+            Image(nsImage: self.screen.image)
         }.frame(width: 400, height: 400)
-    }
-    
-    fileprivate func Q1ToQ4Rect(_ inBounds: CGRect) -> CGRect {
-        let screens = NSScreen.screens
-        guard screens.count > 0,
-              let primaryScreen = screens.first else {
-            return inBounds
-        }
-        
-        var result = inBounds
-        result.origin.y = (inBounds.maxY - primaryScreen.frame.maxY) * -1.0
-        return result;
     }
 }
 
-class Store {
-    var window: NSWindow
-    init(window: NSWindow) {
-        self.window = window
-    }
-}
 
 struct WindowGetter: NSViewRepresentable {
     @Binding var window: NSWindow?
--- playground/Screen2.swift
+++ playground/Screen2.swift
@@ -14,11 +14,57 @@ class Screen2: ObservableObject {
     @Published var origin: (x:CGFloat, y:CGFloat) = (0.0, 0.0)
     @Published var heightWidth: (x:CGFloat, y:CGFloat) = (0.0, 0.0)
     @Published var frame: CGRect = .zero
+    @Published var image = NSImage()
     
-    init(){
-//        NotificationCenter.default.addObserver(forName: NSView.frameDidChangeNotification, object: nil, queue: nil) { (notification) in
-//            print(notification)
-//        }
+    var subscriptions = Set<AnyCancellable>()
+    var window: NSWindow
+    
+    init(window: NSWindow){
+        self.window = window
+        Timer.publish(every: 1, on: .main, in: .default)
+            .autoconnect()
+            .sink { _ in
+                let options = CGWindowListOption(arrayLiteral: CGWindowListOption.excludeDesktopElements, CGWindowListOption.optionOnScreenOnly)
+                let windowListInfo = CGWindowListCopyWindowInfo(options, CGWindowID(0))
+                let infoList = windowListInfo as NSArray? as? [[String: AnyObject]]
+                if let il = infoList {
+                    for win in il {
+                        if let name = win["kCGWindowOwnerName"] as? String,
+                           let bounds = win["kCGWindowBounds"]{
+                            if name == "playground" {
+                                print("Bounds are: \(bounds)")
+                            }
+                        }
+                    }
+                }
+                let windowId = CGWindowID(self.window.windowNumber)
+                let windowOption: CGWindowListOption = .optionOnScreenBelowWindow
+                let imageOption: CGWindowImageOption = .boundsIgnoreFraming
+                
+                if let windowCoordinates = self.window.contentView?.bounds {
+                    let screenCoordinates = self.window.convertToScreen(windowCoordinates)
+                    let q1Toq4Coordinates = self.Q1ToQ4Rect(screenCoordinates)
+                    let image = CGWindowListCreateImage(q1Toq4Coordinates, windowOption, windowId, imageOption)
+                    if let img = image {
+                        self.image = NSImage(cgImage: img, size: .zero)
+                        print(self.image.isValid)
+                    }
+                } 
+            }
+            .store(in: &subscriptions)
+        
+    }
+    
+    fileprivate func Q1ToQ4Rect(_ inBounds: CGRect) -> CGRect {
+        let screens = NSScreen.screens
+        guard screens.count > 0,
+              let primaryScreen = screens.first else {
+            return inBounds
+        }
+        
+        var result = inBounds
+        result.origin.y = (inBounds.maxY - primaryScreen.frame.maxY) * -1.0
+        return result;
     }
     
 }
